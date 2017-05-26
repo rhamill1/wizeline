1a246a0e0a579d9ae921349c1d746c3e35ccd6a7
rhamill1 <rhamill11@gmail.com>
2017-05-26 14:20:06 -0400

    string_compression function passes both tests

--- scripts/string_compression.rb
+++ scripts/string_compression.rb
@@ -1,3 +1,21 @@
 def string_compression(input_string)
-  return "a3bc4def2"
+  compressed_string = ''
+  letter_counter = 0
+
+  input_string.split("").each_with_index do |letter, index|
+
+    if letter != input_string[index + 1]  && letter_counter == 0
+      compressed_string += letter
+    elsif letter != input_string[index + 1]  && letter_counter > 0
+      letter_counter += 1
+      compressed_string += letter += letter_counter.to_s
+      letter_counter = 0
+    else
+      letter_counter += 1
+    end
+
+  end
+
+  return compressed_string
+
 end
