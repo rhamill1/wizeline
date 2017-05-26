e221b7cf59bc5e8276897e56cc481b43fda65195
rhamill1 <rhamill11@gmail.com>
2017-05-26 18:35:24 -0400

    adds second test for seconds_in_line and passes both tests

--- scripts/seconds_in_line.rb
+++ scripts/seconds_in_line.rb
@@ -1,3 +1,37 @@
 def seconds_in_line(starting_position, line)
-  return 5
+
+  tickets_needed = line[starting_position]
+  moving_position = starting_position
+  seconds_elapsed = 0
+
+  while tickets_needed > 0 do
+
+    seconds_elapsed += 1
+
+    if line[0] == 1 && moving_position > 0
+      line.shift
+
+      moving_position -= 1
+
+    elsif line[0] > 1 && moving_position > 0
+      line[0] -= 1
+      line.push(line.shift)
+
+      moving_position -= 1
+
+    elsif line[0] > 1 && moving_position == 0
+      line[0] -= 1
+      line.push(line.shift)
+
+      moving_position = line.length - 1
+      tickets_needed -= 1
+
+    else
+      return seconds_elapsed
+
+    end
+
+  end
+
+
 end
