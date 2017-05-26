2396d2b04ca431236c3ee26c04a971578111f0af
rhamill1 <rhamill11@gmail.com>
2017-05-26 16:45:39 -0400

    adds first test for seconds_in_line function

---
+++ spec/seconds_in_line_spec.rb
@@ -0,0 +1,8 @@
+require './scripts/seconds_in_line.rb'
+
+
+describe 'seconds_in_line(2, [1, 1, 2, 3])' do
+  it 'returns the amount of seconds a customer waits in line' do
+    expect(seconds_in_line(2, [1, 1, 2, 3])).to eq 5
+  end
+end
