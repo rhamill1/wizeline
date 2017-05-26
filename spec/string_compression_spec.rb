f1b731ff77e6390714d82dc5d9b58f4ba403fb72
rhamill1 <rhamill11@gmail.com>
2017-05-26 14:03:09 -0400

    adds and fails second test for string_compression function

--- spec/string_compression_spec.rb
+++ spec/string_compression_spec.rb
@@ -6,3 +6,9 @@ describe 'string_compression("aaabccccdeff")' do
     expect(string_compression('aaabccccdeff')).to eq 'a3bc4def2'
   end
 end
+
+describe 'string_compression("qrrrrssstuvvv")' do
+  it 'it returns a compressed version of the entered string. When multiple consecutive occurences of the same character occur, the sub-string is shortened to the letter followed by a count of occurances' do
+    expect(string_compression('qrrrrssstuvvv')).to eq 'qr4s3tuv3'
+  end
+end
